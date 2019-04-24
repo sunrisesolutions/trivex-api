e9c4ebff205a816037a1f827af8031e10fda7a50
binhle410 <binh@sunrise.vn>
2019-04-24 23:18:18 +0700

    bean playground

---
+++ user/api/src/Controller/BeanPlaygroundController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/bean")
+ */
+class BeanPlaygroundController
+{
+    /**
+     * @Route("/hello", methods="GET")
+     */
+    public function publishMessage(Request $request): Response
+    {
+//        $content = json_decode($request->getContent(), true);
+
+        return new JsonResponse(['hello honey']);
+    }
+}
