0d7732eb4ea14a4e8e03ef7a67759f49f832954b
binhle410 <binh@sunrise.vn>
2019-04-25 10:25:47 +0700

    mailer

--- user/api/config/bundles.php
+++ user/api/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
