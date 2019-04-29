d34595aa6f2794481a307a4adea41e41dcf8c583
binhle410 <binh@sunrise.vn>
2019-04-29 12:29:24 +0700

    org API mailer

--- organisation/api/config/bundles.php
+++ organisation/api/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
