06d95b506dc08c23b2acccbc8a877df1d06d8978
binhle410 <binh@sunrise.vn>
2019-05-03 21:50:16 +0700

    ORG API: use App\Controller\SendEmailToIndividualMember;

--- organisation/api/src/Controller/SendEmailToIndividualMember.php
+++ organisation/api/src/Controller/SendEmailToIndividualMember.php
@@ -5,25 +5,33 @@ namespace App\Controller;
 
 
 use App\Entity\IndividualMember;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 
 class SendEmailToIndividualMember
 {
     private $mailer;
-    public function __construct(\Swift_Mailer $mailer)
+    private $registry;
+
+    public function __construct(Registry $registry, \Swift_Mailer $mailer)
     {
         $this->mailer = $mailer;
+        $this->registry = $registry;
     }
 
     public function __invoke(IndividualMember $data): IndividualMember
     {
 
-        $message = (new \Swift_Message($data->emailSubject))
-            ->setFrom('no-reply@magentapulse.com')
-            ->setTo($data->emailTo)
-            ->setBody(
-                $data->emailBody,
-                'text/html'
-            )/*
+        /** @var IndividualMember $member */
+        $member = $this->registry->getRepository(IndividualMember::class)->find($data->emailTo);
+        if (!empty($member)) {
+            if (!empty($toEmail = $member->getPerson()->getEmail())) {
+                $message = (new \Swift_Message($data->emailSubject))
+                    ->setFrom('no-reply@magentapulse.com')
+                    ->setTo($toEmail)
+                    ->setBody(
+                        $data->emailBody,
+                        'text/html'
+                    )/*
                  * If you also want to include a plaintext version of the message
                 ->addPart(
                     $this->renderView(
@@ -33,9 +41,11 @@ class SendEmailToIndividualMember
                     'text/plain'
                 )
                 */
-        ;
+                ;
 
-        $this->mailer->send($message);
+                $this->mailer->send($message);
+            }
+        }
         return $data;
     }
 }
