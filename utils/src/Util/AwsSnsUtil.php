c77ff131fa10066610faa2082d8954ac50a98c83
binhle410 <binh@sunrise.vn>
2019-06-09 00:28:29 +0700

    Utils

--- utils/src/Util/AwsSnsUtil.php
+++ utils/src/Util/AwsSnsUtil.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace App\Util;
 
 use App\Message\Message;
+use Aws\Exception\AwsException;
 use Aws\Sdk;
+use Aws\Sns\SnsClient;
 use Aws\Sqs\SqsClient;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class AwsSnsUtil
 {
-    /** @var SqsClient */
+    /** @var SnsClient */
     private $client;
     private $sdk;
     private $applicationName;
@@ -35,6 +37,38 @@ class AwsSnsUtil
     public function getTopicArn()
     {
         return getenv('AWS_SNS_PREFIX').AppUtil::PROJECT_NAME.'_'.AppUtil::APP_NAME.'_'.strtoupper(getenv('APP_ENV'));
+    }
+
+    public function createTopic($name)
+    {
+        return $this->client->createTopic([
+            'Name' => $name]);
+    }
+
+    public function deleteTopic($arn)
+    {
+        try {
+            $result = $this->client->deleteTopic([
+                'TopicArn' => $arn,
+            ]);
+            var_dump($result);
+        } catch (AwsException $e) {
+            // output error message if fails
+            error_log($e->getMessage());
+        }
+        return $result;
+    }
+
+    public function listTopics(){
+        try {
+            $result = $this->client->listTopics([
+            ]);
+            var_dump($result);
+        } catch (AwsException $e) {
+            // output error message if fails
+            error_log($e->getMessage());
+        }
+        return $result;
     }
 
     public function publishMessage($object, $topicArn = null)
