de63bb5d29be15d5e65487cbd82a3fe30d1e6b34
binhle410 <binh@sunrise.vn>
2019-06-24 14:47:41 +0700

    AUTH remove services.yaml

---
+++ authorisation/api/src/Entity/IndividualMember.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={
+ *         "get"={"access_control"="is_granted('ROLE_USER')"},
+ *     },
+ *     normalizationContext={"groups"={"read"}},
+ *     denormalizationContext={"groups"={"write"}}
+ * )
+ * @ORM\Entity(repositoryClass="App\Repository\IndividualMemberRepository")
+ * @ORM\Table(name="authorisation__individual_member")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class IndividualMember
+{
+    /**
+     * @var int|null The User Id
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer",options={"unsigned":true})
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @Groups("read")
+     * @ORM\Column(type="string", length=191, nullable=true)
+     */
+    private $uuid;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Organisation", inversedBy="individualMembers")
+     * @ORM\JoinColumn(name="id_organisation", referencedColumnName="id")
+     */
+    private $organisation;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Person", inversedBy="individualMembers")
+     * @ORM\JoinColumn(name="id_person", referencedColumnName="id")
+     */
+    private $person;
+
+    public function __construct()
+    {
+
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(?string $uuid): self
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getOrganisation(): ?Organisation
+    {
+        return $this->organisation;
+    }
+
+    public function setOrganisation(?Organisation $organisation): self
+    {
+        $this->organisation = $organisation;
+
+        return $this;
+    }
+
+    public function getPerson(): ?Person
+    {
+        return $this->person;
+    }
+
+    public function setPerson(?Person $person): self
+    {
+        $this->person = $person;
+
+        return $this;
+    }
+
+}
--- authorisation/api/src/Entity/Organisation.php
+++ authorisation/api/src/Entity/Organisation.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -17,6 +19,16 @@ class Organisation
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    public function __construct()
+    {
+        $this->individualMembers = new ArrayCollection();
+    }
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\IndividualMember", mappedBy="organisation")
+     */
+    private $individualMembers;
 
     /**
      * @ORM\Column(type="string", length=191, nullable=true)
@@ -53,6 +65,37 @@ class Organisation
     public function setName(?string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|IndividualMember[]
+     */
+    public function getIndividualMembers(): Collection
+    {
+        return $this->individualMembers;
+    }
+
+    public function addIndividualMember(IndividualMember $individualMember): self
+    {
+        if (!$this->individualMembers->contains($individualMember)) {
+            $this->individualMembers[] = $individualMember;
+            $individualMember->setOrganisation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIndividualMember(IndividualMember $individualMember): self
+    {
+        if ($this->individualMembers->contains($individualMember)) {
+            $this->individualMembers->removeElement($individualMember);
+            // set the owning side to null (unless already changed)
+            if ($individualMember->getOrganisation() === $this) {
+                $individualMember->setOrganisation(null);
+            }
+        }
 
         return $this;
     }
