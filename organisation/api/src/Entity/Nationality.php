bbefbd619ba7b4d710b63dfc0560dd852d7dbddb
binhle410 <binh@sunrise.vn>
2019-07-02 22:32:48 +0700

    MISC

--- organisation/api/src/Entity/Nationality.php
+++ organisation/api/src/Entity/Nationality.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\NationalityRepository")
- * @ORM\Table(name="person__nationality")
+ * @ORM\Table(name="organisation__nationality")
  */
 class Nationality
 {
