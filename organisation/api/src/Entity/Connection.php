aac5d4198b603892cb08807705d8d761282a6c41
binhle410 <binh@sunrise.vn>
2019-08-10 20:44:12 +0800

    CONNECTION

--- organisation/api/src/Entity/Connection.php
+++ organisation/api/src/Entity/Connection.php
@@ -103,11 +103,9 @@ class Connection
      */
     public function getPersonData()
     {
-        /** @var Person $fromPerson */
-        $fromPerson = $this->fromMember->getPerson();
-        /** @var Person $fromPerson */
-        $toPerson = $this->toMember->getPerson();
-        return ['from' => ['name' => $fromPerson->getName()], 'to' => ['name' => $toPerson->getName()],
+        $fromPerson = $this->fromMember->getPersonData();
+        $toPerson = $this->toMember->getPersonData();
+        return ['from' => $fromPerson, 'to' => $toPerson,
         ];
     }
 
