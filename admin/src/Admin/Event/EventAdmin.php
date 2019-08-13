178e6b8e4dd114e11ee93935d619fbe9a9d55d2b
binhle410 <binh@sunrise.vn>
2019-08-13 10:04:25 +0800

    OK

--- admin/src/Admin/Event/EventAdmin.php
+++ admin/src/Admin/Event/EventAdmin.php
@@ -131,12 +131,12 @@ class EventAdmin extends BaseAdmin
             ->add('startedAt', DateTimePickerType::class, ['label' => 'form.label_started_at',
 //                'locale' => 'en_SG',
                 'view_timezone' => 'Asia/Singapore',
-                'format' => 'd M Y, H:mm:sm'
+                'format' => 'd M Y, H:mm:ss'
                 ])
             ->add('endedAt', DateTimePickerType::class, ['label' => 'form.label_ended_at',
 //                'locale' => 'en_SG',
                 'view_timezone' => 'Asia/Singapore',
-                'format' => 'd M Y, H:mm:sm'
+                'format' => 'd M Y, H:mm:ss'
                 ])
             ->add('timezone', ChoiceType::class, array(
                 'required' => false,
