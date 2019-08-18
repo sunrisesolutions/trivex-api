b1ba6dcfa301092b4a4970f9017d592ccc5ede1e
binhle410 <binh@sunrise.vn>
2019-08-18 21:45:22 +0800

    MSG and ADMIN

--- messaging/api/src/Filter/NotLikeFilter.php
+++ messaging/api/src/Filter/NotLikeFilter.php
@@ -21,7 +21,7 @@ final class NotLikeFilter extends AbstractContextAwareFilter
             if (!empty($value)) {
 //                $alias = 'messageSender';
 //                $queryBuilder->join('message.sender', 'messageSender');
-                [$alias, $field, $associations] = $this->addJoinsForNestedProperty('message.sender', $rootAlias, $queryBuilder, $queryNameGenerator, $resourceClass);
+                [$alias, $field, $associations] = $this->addJoinsForNestedProperty('message.sender.uuid', $rootAlias, $queryBuilder, $queryNameGenerator, $resourceClass);
 
                 $queryBuilder->andWhere($expr->notLike($alias.'.uuid', $expr->literal($value)));
             }
