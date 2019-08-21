fa945412e4d2b6c83c6ac3b61943ec80c4ac8f24
binhle410 <binh@sunrise.vn>
2019-08-21 12:00:39 +0800

    August 21 2019

--- messaging/api/src/Entity/MessageOption.php
+++ messaging/api/src/Entity/MessageOption.php
@@ -14,7 +14,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 /**
  * @ApiResource(
  *     attributes={"access_control"="is_granted('ROLE_USER')"},
- *     normalizationContext={"groups"={"read_set_from_option", "read_message", "read_option"}},
+ *     normalizationContext={"groups"={"read_set_from_option", "read_option"}},
  *     denormalizationContext={"groups"={"write_set_from_option"}}
  * )
  * @ORM\Entity(repositoryClass="App\Repository\MessageOptionRepository")
